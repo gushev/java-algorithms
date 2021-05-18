09cf6c9ad3f9abd59567c227e20094c5e6fa889a
Georgi Ushev <ushev.georgi@gmail.com>
2021-05-18 12:18:55 +0300

    Display change in Insertion Sort

--- src/sorting/insertionsort/InsertionSort.java
+++ src/sorting/insertionsort/InsertionSort.java
@@ -1,7 +1,18 @@
 package sorting.insertionsort;
 
+import java.util.Arrays;
+
 public class InsertionSort {
-  public int[] sort(int[] inputArray) {
+
+  private int[] inputArray;
+
+  public InsertionSort(int[] inputArray) {
+    this.inputArray = inputArray;
+  }
+
+  public int[] sort() {
+
+    System.out.printf("Initial array: %s%n", Arrays.toString(inputArray));
 
     for (int i = 1; i < inputArray.length; i++) { // starting the iteration from the second element
 
@@ -10,15 +21,31 @@ public class InsertionSort {
 
       // loop while previous key is higher than 0 and its value is higher than the current value
       while (prevKey >= 0 && inputArray[prevKey] > curr) {
+        printChange(prevKey);
         inputArray[prevKey + 1] = inputArray[prevKey]; // move the previous key value to the right
+        inputArray[prevKey] = curr; // move current value element to the left
         prevKey -= 1; // decrement the previous key with one, so we can go one element to the left
       }
-
-      // move the current element to the right of where the element that
-      // we've identified as smaller in the while loop
-      inputArray[prevKey + 1] = curr;
     }
 
+    System.out.printf("Output array: %s%n", Arrays.toString(inputArray));
     return inputArray;
+  }
+
+  private void printChange(int prevKey) {
+    StringBuilder change = new StringBuilder("[");
+
+    for (int i = 0; i < inputArray.length; i++) {
+      change.append(inputArray[i]);
+      if (prevKey == i) {
+        change.append(" <-> ");
+      } else if (i < inputArray.length - 1) {
+        change.append(", ");
+      }
+    }
+
+    change.append("]");
+
+    System.out.println(change);
   }
 }
--- src/sorting/insertionsort/Main.java
+++ src/sorting/insertionsort/Main.java
@@ -6,8 +6,8 @@ public class Main {
   public static void main(String[] args) {
     int[] numbers = {5, 2, 4, 6, 1, 3};
 
-    InsertionSort insertionSort = new InsertionSort();
+    InsertionSort insertionSort = new InsertionSort(numbers);
 
-    System.out.println(Arrays.toString(insertionSort.sort(numbers)));
+    insertionSort.sort();
   }
 }
